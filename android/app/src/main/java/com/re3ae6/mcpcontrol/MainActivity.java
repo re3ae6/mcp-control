303070c699b81c5ff46cfc2a733aa660c80722ea
re3ae6 <re3ae6@gmail.com>
2026-09-25 19:57:07 +0330

    Fix monitor service Intent import

--- android/app/src/main/java/com/re3ae6/mcpcontrol/MainActivity.java
+++ android/app/src/main/java/com/re3ae6/mcpcontrol/MainActivity.java
@@ -2,6 +2,7 @@ package com.re3ae6.mcpcontrol;
 
 import android.app.Activity;
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
